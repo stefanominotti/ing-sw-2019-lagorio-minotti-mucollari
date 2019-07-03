3ce476861ab32b7158240b86a478de9d40c73fb5
Stefano Minotti <stefanominotti1997@icloud.com>
2019-07-03 21:25:21 +0200

    Revert "git bug"
    This reverts commit 7f456cde59b83b8c09a4f0888a865211c9a314b8.

--- src/main/java/it/polimi/se2019/server/RMIVirtualClient.java
+++ src/main/java/it/polimi/se2019/server/RMIVirtualClient.java
@@ -13,6 +13,7 @@ public class RMIVirtualClient extends Thread implements VirtualClientInterface {
     private final RMIClientInterface client;
     private RMIProtocolServer server;
     private boolean active;
+    private long pingTime;
 
     /**
      * Class constructor, it builds a RMI virtual client
@@ -43,6 +44,8 @@ public class RMIVirtualClient extends Thread implements VirtualClientInterface {
      */
     @Override
     public void run() {
+        this.pingTime = System.currentTimeMillis();
+
         while(this.active) {
 
             if (System.currentTimeMillis() - this.pingTime > 5000) {
@@ -61,6 +64,10 @@ public class RMIVirtualClient extends Thread implements VirtualClientInterface {
                 Thread.currentThread().interrupt();
             }
         }
+    }
+
+    void ping() {
+        this.pingTime = System.currentTimeMillis();
     }
 
     /**
